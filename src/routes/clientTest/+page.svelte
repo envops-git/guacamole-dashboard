108f2875565f99e0795f5215ac7366a2ba97f72d
envops-git <biranzegelman97@gmail.com>
2022-08-22 17:38:39 +0300

    added mouse and keyboard support

--- src/routes/clientTest/+page.svelte
+++ src/routes/clientTest/+page.svelte
@@ -11,5 +11,23 @@
 
 		document.body.appendChild(client.getDisplay().getElement());
 		client.connect('token=' + data.token);
+
+		let mouse = new Guacamole.Mouse(client.getDisplay().getElement());
+
+		mouse.onmousedown =
+			mouse.onmouseup =
+			mouse.onmousemove =
+				function (mouseState) {
+					client.sendMouseState(mouseState);
+				};
+		let keyboard = new Guacamole.Keyboard(document);
+
+		keyboard.onkeydown = function (keysym) {
+			client.sendKeyEvent(1, keysym);
+		};
+
+		keyboard.onkeyup = function (keysym) {
+			client.sendKeyEvent(0, keysym);
+		};
 	});
 </script>
