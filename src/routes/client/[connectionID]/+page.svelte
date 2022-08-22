ea7d83658248534c2748f40cc883661bfd53fc70
envops-git <biranzegelman97@gmail.com>
2022-08-23 00:54:20 +0300

    update

--- src/routes/client/[connectionID]/+page.svelte
+++ src/routes/client/[connectionID]/+page.svelte
@@ -23,14 +23,13 @@
 	let client;
 	let tunnel;
 
+	let displayWidth;
+	let displayHeight;
+
 	let origHeight;
 	let origWidth;
 
 	async function loadPage() {
-		origHeight = window.screen.height - 50;
-		origWidth = window.screen.width;
-		console.log(origHeight);
-		console.log(origWidth);
 		try {
 			const response = await fetch('/api/connections/token/' + data.connectionID);
 
@@ -47,14 +46,21 @@
 				if (state == 5) {
 					location.assign('/');
 				}
-				console.log(state)
-				console.log(client.getDisplay().getHeight(),client.getDisplay().getWidth());
-			}
+				console.log(state);
+				console.log(client.getDisplay().getHeight(), client.getDisplay().getWidth());
+			};
+
+			origHeight = (displayHeight - 50) * window.devicePixelRatio;
+			origWidth = displayWidth * window.devicePixelRatio;
+			console.log(origHeight);
+			console.log(origWidth);
 
 			document.getElementById('display').appendChild(client.getDisplay().getElement());
 			client.connect('token=' + token + '&width=' + origWidth + '&height=' + origHeight);
-			// client.getDisplay().scale(scale);
-			
+			scale = Math.min(displayWidth / origWidth, displayHeight - 50 / origHeight);
+			scale += scale / 10;
+			client.getDisplay().scale(scale);
+
 			let mouse = new Guacamole.Mouse(client.getDisplay().getElement());
 			mouse.onmouseup = (state) => client.sendMouseState(state);
 			mouse.onmousedown = (state) => client.sendMouseState(state);
@@ -82,11 +88,21 @@
 </script>
 
 <svelte:window
+	bind:innerWidth={displayWidth}
+	bind:innerHeight={displayHeight}
 	on:resize={() => {
-		console.log('Original Height ' + origHeight, 'Current Height ' + (window.screen.height - 50), 'Scale' + ((window.screen.height - 50) / origHeight));
-		console.log('Original Width ' + origWidth, 'Current Width ' + window.screen.width, 'Scale' + (window.screen.width / origWidth));
+		console.log(
+			'Original Height ' + origHeight,
+			'Current Height ' + (displayHeight - 50),
+			'Scale' + (displayHeight - 50) / origHeight
+		);
+		console.log(
+			'Original Width ' + origWidth,
+			'Current Width ' + displayWidth,
+			'Scale' + displayWidth / origWidth
+		);
 		if (loaded) {
-			scale = Math.min(window.screen.width / origWidth, window.screen.height - 50 / origHeight);
+			scale = Math.min(displayWidth / origWidth, displayHeight - 50 / origHeight);
 			scale += scale / 10;
 			client.getDisplay().scale(scale);
 		}
