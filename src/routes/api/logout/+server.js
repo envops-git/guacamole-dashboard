7dcf5ea1b66e7abf913daf4eb66cae733c3367d8
envops-git <biranzegelman97@gmail.com>
2022-08-22 12:56:03 +0300

    prerender false

--- src/routes/api/logout/+server.js
+++ src/routes/api/logout/+server.js
@@ -2,5 +2,7 @@ export async function GET(event) {
   const setCookieHeaders = new Headers();
   setCookieHeaders.append('set-cookie', `accessToken=; Max-Age=0; SameSite=Strict; Path=/; HttpOnly; Secure;`);
   setCookieHeaders.append('set-cookie', `dataSource=; Max-Age=0; SameSite=Strict; Path=/; HttpOnly; Secure;`);
+  setCookieHeaders.append('set-cookie', `username=; Max-Age=0; SameSite=Strict; Path=/; HttpOnly; Secure;`);
+
   return new Response(undefined, { status: 204, headers: setCookieHeaders })
 }
--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,15 @@
 import adapter from '@sveltejs/adapter-node';
-
 import preprocess from "svelte-preprocess";
 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	},
-	preprocess: [
+  kit: {
+    adapter: adapter(),
+    prerender: { enabled: false },
+
+  },
+  preprocess: [
     preprocess({
       postcss: true,
     }),
